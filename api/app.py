92a339764659211ad1f1e46c98ebe689da188b22
Reto Schürmann <reto.schuermann@gmail.com>
2021-12-11 15:50:11 +0100

    Bootstrap page added

--- api/app.py
+++ api/app.py
@@ -2,47 +2,50 @@ from flask import Flask
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
+from flask import render_template
 
 app = Flask(__name__)
+
 
 model = load_model("./model/model2.h5")
 print(model.summary())
 
-# Give the link of the image here to test
-test_image1 = image.load_img("./tests/pictures/frog.jpeg", target_size=(32, 32))
-test_image = image.img_to_array(test_image1)
-test_image = np.expand_dims(test_image, axis=0)
-result = model.predict(test_image)
 
-print(result)
-if result[0][0] == 1:
-    print("Aeroplane")
-elif result[0][1] == 1:
-    print("Automobile")
-elif result[0][2] == 1:
-    print("Bird")
-elif result[0][3] == 1:
-    print("Cat")
-elif result[0][4] == 1:
-    print("Deer")
-elif result[0][5] == 1:
-    print("Dog")
-elif result[0][6] == 1:
-    print("Frog")
-elif result[0][7] == 1:
-    print("Horse")
-elif result[0][8] == 1:
-    print("Ship")
-elif result[0][9] == 1:
-    print("Truck")
-else:
-    print("Error")
+@app.route("/")
+def index():
+    # Give the link of the image here to test
+    test_image1 = image.load_img("./tests/pictures/horse.jpeg", target_size=(32, 32))
+    test_image = image.img_to_array(test_image1)
+    test_image = np.expand_dims(test_image, axis=0)
+    result = model.predict(test_image)
+
+    print(result)
+    if result[0][0] == 1:
+        print("Aeroplane")
+    elif result[0][1] == 1:
+        print("Automobile")
+    elif result[0][2] == 1:
+        print("Bird")
+    elif result[0][3] == 1:
+        print("Cat")
+    elif result[0][4] == 1:
+        print("Deer")
+    elif result[0][5] == 1:
+        print("Dog")
+    elif result[0][6] == 1:
+        print("Frog")
+    elif result[0][7] == 1:
+        print("Horse")
+    elif result[0][8] == 1:
+        print("Ship")
+    elif result[0][9] == 1:
+        print("Truck")
+    else:
+        print("Error")
+
+    #   return jsonify({'message':'ok','prediction':prediction_result}),200
+    return render_template("template.html")
 
 
-# @app.route('/')
-#
-# def index():
-#     return jsonify({'message':'ok'}),200
-#
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == "__main__":
+    app.run()
